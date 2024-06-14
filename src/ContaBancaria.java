b9e55a44c93dffb5ad18be0eeed290ef9cae3989
Bg <diegobds441@gmail.com>
2024-06-14 00:53:12 -0300

    Atualização

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,28 +1,65 @@
 import java.util.Scanner;
-import java.util.Locale;
 
 public class ContaBancaria {
     public static void main(String[] args) throws Exception {
-        Scanner banco = new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        Boolean continuar = true;
 
-        System.out.println("Informe Seu primeiro Nome:");
-        String nome = banco.next();
+        while (continuar) {
+            System.out.println("----------------------------------------");
+            System.out.println("Seja Bem Vindo ao Nosso Banco!");
+            System.out.println("Escolha uma das Opção:");
+            System.out.println("1 - Despositar");
+            System.out.println("2 - Sacar");
+            System.out.println("3 - Consultar Saldo");
+            System.out.println("0 - encerrar");
+            int opcao = scanner.nextInt();
+            System.out.println("----------------------------------------");
 
-        System.out.println("Informe seu segundo Nome:");
-        String sobrenome = banco.next();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o valor que deseja Depositar");
+                    double saldoDepositado = scanner.nextDouble();
+                    if (saldoDepositado > 0) {
+                        saldo += saldoDepositado;
+                        System.out.println("Novo Saldo de: R$ " + saldo);
+                    } else {
+                        System.out.println("Não foi possivel depositar o valor de: R$ " + saldoDepositado);
+                    }
 
-        System.out.println("Infome sua Idade:");
-        int idade = banco.nextInt();
+                    break;
+                case 2:
+                    System.out.println("Digite o valor que deseja sacar: ");
+                    double valorSaque = scanner.nextDouble();
+                    if (valorSaque > 0 && valorSaque < saldo) {
+                        saldo -= valorSaque;
+                        System.out.println("Saque realizado de: R$ " + valorSaque);
+                        System.out.println("Saldo Atual: R$ " + saldo);
+                    } else if (valorSaque > saldo) {
+                        System.out.println("Saldo insuficiente");
+                    } else {
+                        System.out.println("Valor Invalido");
+                    }
 
-        System.out.println("Valor que deseja colocar de inicio:");
-        int valor = banco.nextInt();
+                    break;
 
-        System.out.println("Suas Informação São: ");
-        System.out.println("------------------------------------");
-        System.out.println("Nome Completo: " + nome + " " + sobrenome);
-        System.out.println("Idade: " + idade);
-        System.out.println("valor que tem na conta: " + valor);
-        System.out.println("------------------------------------");
+                case 3:
+                    System.out.println("Saldo da conta: R$ " + saldo);
 
+                    break;
+
+                case 0:
+                    System.out.println("Programa Encerrado!");
+                    continuar = false;
+
+                    break;
+
+                default:
+                    System.out.println("Opção Inválida. Tente Novamente.");
+                    break;
+            }
+        }
+        scanner.close();
     }
 }
